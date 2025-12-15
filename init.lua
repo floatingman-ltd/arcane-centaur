55df939277226bf595734e8141bc311571e6cb15
walt <2963975+waltiam@users.noreply.github.com>
2025-12-15 16:22:56 +0000

    lazyvim repo cloned and cleaned

--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
-require('options') -- loads options.lua
-require('loader')  -- loads the loader/init.lua (lazy)
-require('keymaps') -- loads keymaps.lua
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/plugins/leaf.lua
+++
@@ -1,8 +0,0 @@
-return { 
-  "daschw/leaf.nvim" ,
-  theme = "light",
-  contrast = "medium",
-  config = function()
-    vim.cmd([[colorscheme leaf]])
-  end,
-}
