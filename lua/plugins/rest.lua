4ca97f0413f9e5476b8d1b274fabc8759922542d
walt <2963975+waltiam@users.noreply.github.com>
2026-03-25 10:14:33 -0400

    the documented install plan

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,9 +1,12 @@
 return {
   {
     "rest-nvim/rest.nvim",
-    ft = { "http" },
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
+    "nvim-treesitter/nvim-treesitter",
+      opts = function (_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    }
+  }
 }
